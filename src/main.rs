8170798564941e13cbb19dc5a99063344f919a19
Hazel OHearn <gygaxis@protonmail.com>
2021-04-06 18:53:41 -0300

    Regex validates name, Arguments:, Result:, and Examples: as all existant

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 use regex::Regex;
 
 fn main() {
-    let validator = Regex::new(r"(?ms)\A(z_)?[[:alpha:]]+\n.*?(Result:\n");
+    let validator = Regex::new(r"(?ms)\A(z_)?[[:alpha:]]+ .*?(Arguments:\n.*?)+(Result:\n.*?)+(Examples:\n.*?)+").unwrap();
+    let example1 =
+        std::fs::read_to_string("quizface_help/z_getoperationresult.txt")
+            .unwrap();
+    let example2 =
+        std::fs::read_to_string("quizface_help/getaddressbalance.txt").unwrap();
+    let example3 =
+        std::fs::read_to_string("quizface_help/settxfee.txt").unwrap();
+    dbg!(validator.is_match(&example1));
+    dbg!(validator.is_match(&example2));
+    dbg!(validator.is_match(&example3));
 }
